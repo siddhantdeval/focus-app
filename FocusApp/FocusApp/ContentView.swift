c43eaaa0cfdc51e5f3f563cffee417ad97f7b99e
SiddhantDeval <siddhantdeval15@gmail.com>
2026-03-09 14:15:55 +0530

    feat: initialize new SwiftUI macOS application project

---
+++ FocusApp/FocusApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusApp
+//
+//  Created by Siddhant Deval on 09/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusApp/FocusApp/FocusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusAppApp.swift
+//  FocusApp
+//
+//  Created by Siddhant Deval on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
